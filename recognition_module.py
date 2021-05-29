aea90bc76b633d58ba434d5d11ce176ba692a9a3
Mahii143 <2012091@nec.edu.in>
2021-05-29 23:34:24 +0530

    final commit

--- recognition_module.py
+++ recognition_module.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from datetime import date
 
 today = date.today()
-print("Today's date:", today)
+#print("Today's date:", today)
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
@@ -19,11 +19,11 @@ def recorder():
         
         # read the audio data from the default microphone
         audio_data = r.record(source, duration=10)
-        print("Recognizing...")
+        #print("Recognizing...")
         
         # convert speech to text
         text = r.recognize_google(audio_data)
-        print("user said: {0}".format(text))
+        #print("user said: {0}".format(text))
 
 
         #os.remove("mahir.txt")
